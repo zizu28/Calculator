59a188d5b40b4b83423fdfec36d76b0dcb8efc01
zizu28 <ziblimzulka.zz@gmail.com>
2023-08-17 03:10:36 +0700

    Logic implementation

---
+++ parser.js
@@ -0,0 +1,201 @@
+let operatorList = {
+    '(': {
+      type: 'bracket',
+      text: 'bracket',
+      precedence: 1
+    },
+    ')': {
+      type: 'bracket',
+      text: 'bracket',
+      precedence: 1
+    },
+    '+': {
+      type: 'operator',
+      text: 'add',
+      precedence: 2
+    },
+    '-': {
+      type: 'operator',
+      text: 'subtract',
+      precedence: 2
+    },
+    've': {
+      type: 'operator',
+      text: 'negative',
+      precedence: 3
+    },
+    'pl': {
+      type: 'operator',
+      text: 'positive',
+      precedence: 3
+    },
+    '*': {
+      type: 'operator',
+      text: 'multiply',
+      precedence: 4
+    },
+    '/': {
+      type: 'operator',
+      text: 'divide',
+      precedence: 4
+    },
+    '^': {
+      type: 'function',
+      text: 'exponent',
+      precedence: 5
+    },
+    '!': {
+      type: 'function',
+      text: 'factorial',
+      precedence: 6
+    },
+    'sin': {
+      type: 'function',
+      text: 'sin',
+      precedence: 7
+    },
+    'cos': {
+      type: 'function',
+      text: 'cos',
+      precedence: 7
+    },
+    'tan': {
+      type: 'function',
+      text: 'tan',
+      precedence: 7
+    },
+    '√': {
+      type: 'function',
+      text: 'sqrt',
+      precedence: 5
+    },
+  }
+  
+  function Node(x) {
+    this.node = x
+    this.parent = null
+    this.leftChild = null
+    this.rightChild = null
+    this.text = isNaN(this.node) ? operatorList[x].text : 'constant'
+    this.type = isNaN(this.node) ? operatorList[x].type : 'constant'
+    this.precedence = isNaN(this.node) ? operatorList[x].precedence : 7
+  }
+  
+  const parse = (expr) => {
+    let nodes = [], prev = null, nums = 0
+  
+    while(expr) {
+      let match = expr.match(/^[\d.]+/) || expr.match(/^[a-z]+/)
+      
+      if (match) {
+        if (isNaN(match[0])) {
+          if (match[0].includes('.'))
+            throw 'SyntaxError' // Decimal point is more than 1
+  
+          nodes.push(new Node(match[0]))
+          expr = expr.replace(/^[a-z]+/, '')
+        } else {         
+          // for cases like (3)3 and sin(30)3
+          if (prev && prev === ')') {
+            throw 'SyntaxError'
+          }
+  
+          nodes.push(new Node(match[0]))
+          expr = expr.replace(/^[\d.]+/, '')
+  
+          nums++
+        }
+  
+        prev = match[0]
+      } else {
+        let char = expr.charAt(0)
+        
+        // for cases like 3(3) and 3sin(30)
+        if (prev && !isNaN(prev) && '(√sincostan'.includes(char)) {
+          nodes.push(new Node('*'))
+        } 
+  
+        // for cases like 3(3)√4 and sin(30)cos(30)
+        // for cases like (3)(3) and sin(30)(3)
+        if (prev && prev === ')' && '√sincostan'.includes(char)) {
+          nodes.push(new Node('*'))
+        } else if (prev && prev === ')' && char === '(') {
+          nodes.push(new Node('*'))
+        }
+        
+        if ('+-*/'.includes(prev) && '*/'.includes(char)) {
+          throw 'SyntaxError'
+        }
+  
+        if (char === '-' && !'()'.includes(prev) && isNaN(prev)) {
+          nodes.push(new Node('ve'))
+        } else if (char === '+' && !'()'.includes(prev) && isNaN(prev)) {
+          nodes.push(new Node('pl'))
+        } else {
+          nodes.push(new Node(char))
+        }      
+  
+        expr = expr.replace(char, '')
+        prev = char
+      }
+    }   
+    
+    // Should contain numbers
+    if (nums === 0)
+      throw 'SyntaxError' 
+  
+    console.log(nodes)
+  
+    return createTree(nodes)  
+  }
+  
+  const createTree = (nodes) => {
+    let expressionTree = nodes.reduce((tree, node) => insertToTree(tree, node), new Node('('))
+    
+    while(expressionTree.parent) {
+      expressionTree = expressionTree.parent
+    }
+  
+    expressionTree = expressionTree.rightChild
+    expressionTree.parent = null
+    
+    return expressionTree
+  }
+  
+  const insertToTree = (currentNode, newNode) => {
+    let condition = newNode.node === '^' || newNode.node === '√'
+                  ? newNode.precedence < currentNode.precedence 
+                  : newNode.precedence <= currentNode.precedence
+  
+    if (newNode.node === ')') {
+      return deleteNode(currentNode)
+    }
+  
+    if (newNode.text !== 'positive' && newNode.text !== 'negative' && newNode.type !== 'bracket' && condition) {   
+      return insertToTree(currentNode.parent, newNode)
+    } else {
+      newNode.leftChild = currentNode.rightChild
+      currentNode.rightChild = newNode
+      newNode.parent = currentNode
+      
+      if (newNode.leftChild) {
+        newNode.leftChild.parent = newNode
+      }
+  
+      return newNode
+    }
+  }
+  
+  const deleteNode = (currentNode) => {
+    let openingParen = climbTree(currentNode)
+    openingParen.rightChild.parent = openingParen.parent
+    openingParen.parent.rightChild = openingParen.rightChild
+    return openingParen.parent
+  }
+  
+  const climbTree = (currentNode) => {
+    if (currentNode.parent.type !== 'bracket') {
+      return climbTree(currentNode.parent)
+    }
+    return currentNode.parent
+  }
--- script.js
+++ script.js
@@ -1,16 +1,76 @@
-const add = (a, b) => a + b
-const substract = (a, b) => a - b
-const divide = (a, b) => {
-    if(b !== 0)
-        return a / b
+let expression = document.getElementById('expression')
+let answer = document.getElementById('answer')
+let equalsButton = document.getElementById('equals')
+
+let previousAnswer = 'null', previousAction = 'null'
+
+const clearContent = () => {
+    expression.textContent = ''
+    answer.textContent = ''
+    previousAction = 'clear'
 }
-const multiply = (a, b) => a * b
-let firstNumber
-let secondNumber
-let operator
-const operate = (firstNumber,operator,secondNumber) => {
-    if(operator === '+') return add(firstNumber,secondNumber)
-    if(operator === '-') return substract(firstNumber,secondNumber)
-    if(operator === '*') return multiply(firstNumber,secondNumber)
-    if(operator === '/') return divide(firstNumber,secondNumber)
+
+const deleteContent = () => {
+    expression.textContent = expression.textContent.slice(0, -1)
+    if(previousAction === 'equals'){
+        answer.textContent = ''
+    }
+    previousAction = 'delete'
 }
+
+const signOperator = () => {
+    expression.textContent += '-'
+    if(previousAction === 'equals'){
+        answer.textContent = ''
+    }
+    previousAction = 'sign'
+}
+
+let allButtons = Array.from(document.querySelectorAll('.buttons>button'))
+allButtons.forEach(button => {
+    if(button.id === 'delete') {
+        button.addEventListener('click', deleteContent)}
+    else if(button.id === 'clear'){
+        button.addEventListener('click', clearContent)}
+    else if(button.id === 'sign'){
+        button.addEventListener('click', signOperator)}
+    else if(button.id !== 'equals'){
+        button.addEventListener('click', () => {
+            if(previousAction === 'equals'){
+                if(isNaN(button.id)){
+                    clearContent()
+                    expression.textContent = `ANS${button.textContent}`  
+                }
+                else{
+                    clearContent()
+                    expression.textContent = button.textContent
+                }
+            }
+            else{
+                expression.textContent += button.textContent
+            }
+            previousAction = `${button.id}`
+        })
+    }
+})
+
+equalsButton.addEventListener('click', () => {
+    expression.textContent = expression.textContent.replace(/—/g, '-')
+    expression.textContent = expression.textContent.replace(/✕/g, '*')
+    expression.textContent = expression.textContent.replace(/÷/g, '/')
+    expression.textContent = expression.textContent.replace(/ANS/, `${previousAnswer}`)
+
+    try{
+        let tree = parse(expression.textContent), ans = evaluate(tree)
+        answer.textContent = `${ans}`.length > 12 ? `${ans.toExponential(10)}` : `${ans}`
+        console.log(tree)
+        previousAnswer = ans
+        previousAction = 'equals'
+    }catch(e){
+        console.log(e)
+        answer.textContent = e.toString()
+        setTimeout(() => {
+            answer.textContent = ''
+          }, 1500)
+    }
+})
